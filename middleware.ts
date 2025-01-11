113e0e79b1c04e1c180c64303af294ae1d6d3650
SaraJiang <wbajxw999@gmail.com>
2025-01-11 21:43:31 +0800

    add back-end hono

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,15 @@
-// import { NextResponse } from "next/server";
-// import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 
-// const isProtectedRoute = createRouteMatcher(['/'])
+// API also can be accessed although not signed in
+const isProtectedRoute = createRouteMatcher([
+  '/',
+  // '/api(.*)',
+])
 
-// export default clerkMiddleware(async (auth, req) => {
-//   if (isProtectedRoute(req)) await auth.protect()
-//   return NextResponse.next();
-// })
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+  return NextResponse.next();
 })
 
 export const config = {
@@ -34,4 +19,23 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     await auth.protect()
+//   }
+// })
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
