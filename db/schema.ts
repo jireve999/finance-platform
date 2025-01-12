7423e309304e6cae5eea7ab77fc1844faa501002
SaraJiang <wbajxw999@gmail.com>
2025-01-12 14:22:25 +0800

    update drizzle-kit to 0.21.1 and @neondatabase/serverless to 0.9.3

--- db/schema.ts
+++ db/schema.ts
@@ -6,3 +6,8 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const update = pgTable("updates", {
+  id: text("id").primaryKey(),
+  name: text("name"),
+});
