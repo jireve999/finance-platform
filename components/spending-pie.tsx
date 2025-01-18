bf498514495b035e465ee6fa6dce2d269321b2e1
jireve999 <wbajxw999@aliyun.com>
2025-01-18 21:52:18 +0800

    add Dashbord pie, radar, radial chart

--- components/spending-pie.tsx
+++ components/spending-pie.tsx
@@ -1,4 +1,4 @@
-import { AreaChart, BarChart, FileSearch, LineChart, Loader2 } from "lucide-react";
+import { FileSearch, Loader2, PieChart, Radar, Target } from "lucide-react";
 import { useState } from "react";
 import {
   Select,
@@ -13,22 +13,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { AreaVariant } from "@/components/area-variant";
-import { BarVariant } from "@/components/bar-variant";
-import { LineVariant } from "@/components/line-variant";
+import { PieVariant } from "@/components/pie-variant";
+import { RadarVariant } from "@/components/radar-variant";
+import { RadialVariant } from "@/components/radial-variant";
 import { Skeleton } from "@/components/ui/skeleton";
 
 
 type Props = {
   data?: {
-    date: string;
-    income: number;
-    expenses: number;
+    name: string;
+    value: number;
   }[];
 };
 
-export const Chart = ({data = []}: Props) => {
-  const [chartType, setChartType] = useState("area");
+export const SpendingPie = ({data = []}: Props) => {
+  const [chartType, setChartType] = useState("pie");
   console.log('chartType', chartType)
 
   const onTypeChange = (type: string) => {
@@ -39,7 +38,7 @@ export const Chart = ({data = []}: Props) => {
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg: item-center justify-between">
         <CardTitle className="text-xl line-clamp-1">
-          Transactions
+          Categories
         </CardTitle>
         <Select
           defaultValue={chartType}
@@ -49,27 +48,27 @@ export const Chart = ({data = []}: Props) => {
             <SelectValue placeholder="Chart type" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="area">
+            <SelectItem value="pie">
               <div className="flex items-center">
-                <AreaChart className="size-4 mr-2 shrink-0" />
+                <PieChart className="size-4 mr-2 shrink-0" />
                 <p className="line-clamp-1">
-                  Area chart
+                  Pie chart
                 </p>
               </div>
             </SelectItem>
-            <SelectItem value="line">
+            <SelectItem value="radar">
               <div className="flex items-center">
-                <LineChart className="size-4 mr-2 shrink-0" />
+                <Radar className="size-4 mr-2 shrink-0" />
                 <p className="line-clamp-1">
-                  Line chart
+                  Radar chart
                 </p>
               </div>
             </SelectItem>
-            <SelectItem value="bar">
+            <SelectItem value="radial">
               <div className="flex items-center">
-                <BarChart className="size-4 mr-2 shrink-0" />
+                <Target className="size-4 mr-2 shrink-0" />
                 <p className="line-clamp-1">
-                  Bar chart
+                  Radial chart
                 </p>
               </div>
             </SelectItem>
@@ -86,9 +85,9 @@ export const Chart = ({data = []}: Props) => {
           </div>
         ) : (
           <>
-            {chartType === "line" && <LineVariant data={data} />}
-            {chartType === "area" && <AreaVariant data={data} />}
-            {chartType === "bar" && <BarVariant data={data} />}
+            {chartType === "pie" && <PieVariant data={data} />}
+            {chartType === "radar" && <RadarVariant data={data} />}
+            {chartType === "radial" && <RadialVariant data={data} />}
           </>
         )}
       </CardContent>
@@ -96,7 +95,7 @@ export const Chart = ({data = []}: Props) => {
   );
 }
 
-export const ChartLoading = () => {
+export const SpendingPieLoading = () => {
   return (
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg: item-center justify-between">
