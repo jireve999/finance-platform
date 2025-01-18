3ae91dfe2df72decb6d68d9c0cf330e9537615f6
jireve999 <wbajxw999@aliyun.com>
2025-01-18 17:30:20 +0800

    add Dashboard chart:Area,Line,Bar

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -95,7 +95,7 @@ export const DataCard = ({
           percentageChange > 0 && "text-green-500",
           percentageChange < 0 && "text-red-500" ,
         )}>
-          {formatPercentage(percentageChange)} from last period
+          {formatPercentage(percentageChange, {addPrefix: true})} from last period
         </p>
       </CardContent>
     </Card>
