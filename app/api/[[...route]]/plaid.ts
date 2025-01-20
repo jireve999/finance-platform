89fd5a2b6130b4ee13a08ed227d533176d103a3e
jireve999 <wbajxw999@aliyun.com>
2025-01-20 18:18:41 +0800

    store Plaid exchange token in DB

--- app/api/[[...route]]/plaid.ts
+++ app/api/[[...route]]/plaid.ts
@@ -1,4 +1,9 @@
+import { z } from "zod";
+import { db } from "@/db/drizzle";
+import { connectedBanks } from "@/db/schema";
+import { createId } from "@paralleldrive/cuid2";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import {
   Configuration,
@@ -7,6 +12,8 @@ import {
   Products,
   CountryCode
 } from "plaid";
+
+
 
 const configuration = new Configuration({
   basePath: PlaidEnvironments.sandbox,
@@ -42,6 +49,39 @@ const app = new Hono()
       });
 
       return c.json({ data: token.data.link_token }, 200);
+    },
+  )
+  .post(
+    "/exchange-public-token",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        publicToken: z.string(),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { publicToken } = c.req.valid("json");
+
+      if(!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const exchange = await client.itemPublicTokenExchange({
+        public_token: publicToken,
+      });
+
+      const [connectedBank] = await db
+        .insert(connectedBanks)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          accessToken: exchange.data.access_token,
+        })
+        .returning();
+
+      return c.json({ data: exchange.data.access_token }, 200);
     },
   );
 
--- features/plaid/components/plaid-connect.tsx
+++ features/plaid/components/plaid-connect.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { useState } from "react";
 import { useMount } from "react-use";
-
+import { usePlaidLink } from "react-plaid-link"
 import { Button } from "@/components/ui/button";
 import { useCreateLinkToken } from "@/features/plaid/api/use-create-link-token";
+import { useExchangePublicToken } from "@/features/plaid/api/use-exchange-public-token";
 
 export const PlaidConnect = () => {
   const [token, setToken] = useState<string | null>(null);
 
   const createLinkToken = useCreateLinkToken();
+  const exchangePublicToken = useExchangePublicToken();
+
   useMount(() => {
     createLinkToken.mutate(undefined, {
       onSuccess: ({data}) => {
@@ -17,9 +20,28 @@ export const PlaidConnect = () => {
     });
   });
 
+  const plaid = usePlaidLink({
+    token: token,
+    onSuccess: (publicToken) => {
+      exchangePublicToken.mutate({
+        publicToken,
+      });
+    },
+    env: "sandbox",
+  });
+
+  const onClick = () => {
+    plaid.open();
+  };
+
+  const isDisabled = 
+    !plaid.ready || 
+    exchangePublicToken.isPending
+
   return (
     <Button
-      disabled={!token}
+      onClick={onClick}
+      disabled={isDisabled}
       size="sm"
       variant="ghost"
     >
